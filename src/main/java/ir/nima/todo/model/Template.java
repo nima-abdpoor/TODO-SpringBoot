db18a4b7cbec628960eb988c92073fbf4cbd17ea
nima_abdpour <nimaabdpoor@gmail.com>
2022-06-20 23:26:45 +0430

    add tempelate java class to handle UI of the application.

---
+++ src/main/java/ir/nima/todo/model/Template.java
@@ -0,0 +1,18 @@
+package ir.nima.todo.model;
+
+public enum Template {
+
+    TODO_CREATE("todo_create"),
+    TODO_DETAIL("todo_detail"),
+    TODO_LIST("todos");
+
+    private final String name;
+
+    Template(String url) {
+        this.name = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
